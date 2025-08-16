adf8ce7e332de9969bdf1fd54cf5137f04743e6c
조영무 <fprh13@gmail.com>
2025-08-16 13:25:14 +0900

    refactor: ResponsDto 개선 작업 #3

--- src/main/java/com/hello/boilerplate/global/dto/ResponseDto.java
+++ src/main/java/com/hello/boilerplate/global/dto/ResponseDto.java
@@ -2,40 +2,52 @@ package com.hello.boilerplate.global.dto;
 
 import org.springframework.http.HttpStatus;
 
+import com.hello.boilerplate.global.exception.ErrorCode;
+
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class ResponseDto<T> {
 
-	private final boolean success;
-	private final String statusName;
+	private final String statusCode;
 	private final String message;
-	private final T response;
+	private final String errorCode;
+	private final T data;
 
-	@Builder
-	private ResponseDto(boolean success, String statusName, String message, T response) {
-		this.success = success;
-		this.statusName = statusName;
+	@Builder(access = AccessLevel.PRIVATE)
+	private ResponseDto(String statusCode, String message, String errorCode, T data) {
+		this.statusCode = statusCode;
 		this.message = message;
-		this.response = response;
+		this.errorCode = errorCode;
+		this.data = data;
 	}
 
-	public static <T> ResponseDto<T> success(HttpStatus status, String message, T data) {
+	public static <T> ResponseDto<T> ofSuccess(HttpStatus status, T data) {
 		return ResponseDto.<T>builder()
-			.success(true)
-			.statusName(status.name())
-			.message(message)
-			.response(data)
+			.statusCode(status.name())
+			.message(null)
+			.errorCode(null)
+			.data(data)
 			.build();
 	}
 
-	public static <T> ResponseDto<T> fail(HttpStatus status, String message) {
+	public static <T> ResponseDto<T> ofSuccess(HttpStatus status, String message, T data) {
 		return ResponseDto.<T>builder()
-			.success(false)
-			.statusName(status.name())
+			.statusCode(status.name())
 			.message(message)
-			.response(null)
+			.errorCode(null)
+			.data(data)
+			.build();
+	}
+
+	public static <T> ResponseDto<T> fromErrorCode(ErrorCode errorCode) {
+		return ResponseDto.<T>builder()
+			.statusCode(errorCode.getHttpStatus().name())
+			.message(errorCode.getMessage())
+			.errorCode(errorCode.name())
+			.data(null)
 			.build();
 	}
 }
