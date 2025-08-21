ed0733dd79f7e314466192e38e3754796aace655
조영무 <fprh13@gmail.com>
2025-08-21 22:23:04 +0900

    feat: 기존 CustomException 추가 #5

---
+++ src/main/java/com/hello/boilerplate/global/exception/CustomExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.hello.boilerplate.global.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.hello.boilerplate.global.dto.ResponseDto;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+	@ExceptionHandler(CustomException.class)
+	protected ResponseEntity<ResponseDto<Object>> handleCustomException(CustomException e) {
+		ErrorCode errorCode = e.getErrorCode();
+		return ResponseEntity.status(errorCode.getHttpStatus()).body(ResponseDto.fromErrorCode(errorCode));
+	}
+}
