530e5433661ae71e5ac3b3b0fe1cc91c9ccc2726
조영무 <fprh13@gmail.com>
2025-08-30 13:28:15 +0900

    feat: MethodArgumentNotValidException 핸들러 추가 #5

--- src/main/java/com/hello/boilerplate/global/exception/CustomExceptionHandler.java
+++ src/main/java/com/hello/boilerplate/global/exception/CustomExceptionHandler.java
@@ -1,6 +1,11 @@
 package com.hello.boilerplate.global.exception;
 
+import static org.springframework.http.HttpStatus.*;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,5 +18,13 @@ public class CustomExceptionHandler {
 	protected ResponseEntity<ResponseDto<Void>> handleCustomException(CustomException e) {
 		ErrorCode errorCode = e.getErrorCode();
 		return ResponseEntity.status(errorCode.getHttpStatus()).body(ResponseDto.fromErrorCode(errorCode));
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	ResponseEntity<ResponseDto<Void>> handleValidationException(MethodArgumentNotValidException e) {
+		FieldError fieldError = e.getBindingResult()
+			.getFieldErrors()
+			.get(e.getBindingResult().getFieldErrors().size() - 1);
+		return ResponseEntity.status(BAD_REQUEST).body(ResponseDto.fromFieldError(fieldError));
 	}
 }
