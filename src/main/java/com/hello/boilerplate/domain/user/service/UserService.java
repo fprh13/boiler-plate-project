28aa1226bb37576f5df880092a0016dba974fb54
조영무 <fprh13@gmail.com>
2025-10-12 18:16:07 +0900

    feat: UserService 회원가입 로직 #24

--- src/main/java/com/hello/boilerplate/domain/user/service/UserService.java
+++ src/main/java/com/hello/boilerplate/domain/user/service/UserService.java
@@ -1,10 +1,23 @@
 package com.hello.boilerplate.domain.user.service;
 
+import com.hello.boilerplate.domain.user.dto.UserRequestDto;
+import com.hello.boilerplate.domain.user.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Transactional
+    public void register(final UserRequestDto.Register request) {
+        String encodedPassword = bCryptPasswordEncoder.encode(request.password());
+        userRepository.save(request.toEntity(encodedPassword));
+    }
 }
