9619825ea165d6fa902028b0c6fe2940d9adbd1a
조영무 <fprh13@gmail.com>
2025-10-15 12:54:52 +0900

    chore: UserController를 RestDocsSupport에 등록 #24

--- src/test/java/com/hello/module/RestDocsSupport.java
+++ src/test/java/com/hello/module/RestDocsSupport.java
@@ -1,6 +1,9 @@
 package com.hello.module;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hello.boilerplate.domain.auth.config.SecurityConfig;
+import com.hello.boilerplate.domain.user.controller.UserController;
+import com.hello.boilerplate.domain.user.service.UserService;
 import com.hello.boilerplate.global.controller.HealthCheckController;
 import com.hello.boilerplate.support.config.RestDocsConfig;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +12,19 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = {
-        HealthCheckController.class
+        HealthCheckController.class,
+        UserController.class
+})
+@Import({
+        SecurityConfig.class,
+        RestDocsConfig.class
 })
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
-@Import(RestDocsConfig.class)
 public abstract class RestDocsSupport {
 
     @Autowired
@@ -27,4 +35,7 @@ public abstract class RestDocsSupport {
 
     @Autowired
     protected ObjectMapper objectMapper;
+
+    @MockitoBean
+    protected UserService userService;
 }
