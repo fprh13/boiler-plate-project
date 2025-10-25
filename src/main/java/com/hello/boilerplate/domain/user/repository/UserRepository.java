cda4b3b7bd0ac0116f5233a64e3105de2857563e
조영무 <fprh13@gmail.com>
2025-10-25 23:02:08 +0900

    feat: UserRepository 조회 메서드 #24

--- src/main/java/com/hello/boilerplate/domain/user/repository/UserRepository.java
+++ src/main/java/com/hello/boilerplate/domain/user/repository/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.hello.boilerplate.domain.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findUserByLoginId(String loginId);
 }
