917b1e0c24634c5e9795ef7a9613f0df378c9332
조영무 <fprh13@gmail.com>
2025-11-04 03:17:40 +0900

    test: Auth API 테스트 작성 #24

---
+++ src/test/java/com/hello/boilerplate/domain/auth/controller/AuthControllerTest.java
@@ -0,0 +1,156 @@
+package com.hello.boilerplate.domain.auth.controller;
+
+import com.epages.restdocs.apispec.ResourceDocumentation;
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.epages.restdocs.apispec.Schema;
+import com.hello.boilerplate.domain.auth.dto.LoginRequestDto;
+import com.hello.boilerplate.domain.auth.dto.LoginResponseDto;
+import com.hello.boilerplate.domain.auth.dto.ReissueResponseDto;
+import com.hello.boilerplate.domain.user.entity.User;
+import com.hello.boilerplate.support.fixture.UserFixture;
+import com.hello.module.RestDocsSupport;
+import jakarta.servlet.http.Cookie;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+class AuthControllerTest extends RestDocsSupport {
+
+    private static final String TEST_ACCESS_TOKEN = "accessabcdefghijklmnopqrstuvwxyz";
+    private static final String TEST_REFRESH_TOKEN = "refreshabcdefghijklmnopqrstuvwxyz";
+
+    @Test
+    void API_로그인() throws Exception {
+        //given
+        User userFixture = UserFixture.USER_FIXTURE_1.create();
+
+        LoginRequestDto requestDto = new LoginRequestDto(userFixture.getLoginId(), userFixture.getPassword());
+        LoginResponseDto responseDto = new LoginResponseDto(TEST_ACCESS_TOKEN, TEST_REFRESH_TOKEN);
+
+        Mockito.when(authService.login(requestDto))
+                .thenReturn(responseDto);
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                post("/auths/login")
+                        .content(objectMapper.writeValueAsString(requestDto))
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        //then
+        actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.statusCode").value("OK"))
+                .andExpect(jsonPath("$.message").isEmpty())
+                .andExpect(jsonPath("$.errorCode").isEmpty())
+                .andExpect(jsonPath("$.data").isEmpty())
+                .andDo(restDocsHandler.document(
+                        ResourceDocumentation.resource(ResourceSnippetParameters.builder()
+                                .tag("Auth")
+                                .summary("로그인")
+                                .description("- 로그인 입니다.")
+                                .requestSchema(Schema.schema("LoginRequestDto"))
+                                .responseSchema(Schema.schema("LoginResponseDto"))
+                                .responseFields(
+                                        fieldWithPath("statusCode").description("상태 코드").type(JsonFieldType.STRING),
+                                        fieldWithPath("message").description("메세지").type(JsonFieldType.NULL),
+                                        fieldWithPath("errorCode").description("에러코드").type(JsonFieldType.NULL),
+                                        fieldWithPath("data").description("데이터").type(JsonFieldType.NULL)
+                                ).responseHeaders(
+                                        headerWithName(HttpHeaders.AUTHORIZATION).description("엑세스 토큰"),
+                                        headerWithName(HttpHeaders.SET_COOKIE).description("재발급 토큰")
+                                ).build())
+                        )
+                );
+    }
+
+    @Test
+    void API_로그아웃() throws Exception {
+        //given
+        Mockito.doNothing().when(authService).logout(anyString());
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                post("/auths/logout")
+                        .header(HttpHeaders.AUTHORIZATION, TEST_ACCESS_TOKEN)
+                        .cookie(new Cookie(REFRESH_TOKEN_COOKIE_NAME, TEST_REFRESH_TOKEN))
+                        .contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.statusCode").value("OK"))
+                .andExpect(jsonPath("$.message").isEmpty())
+                .andExpect(jsonPath("$.errorCode").isEmpty())
+                .andExpect(jsonPath("$.data").isEmpty())
+                .andDo(restDocsHandler.document(
+                                ResourceDocumentation.resource(ResourceSnippetParameters.builder()
+                                        .tag("Auth")
+                                        .summary("로그아웃")
+                                        .description("- 로그아웃 입니다.")
+                                        .requestHeaders(
+                                                headerWithName(HttpHeaders.AUTHORIZATION).description("엑세스 토큰")
+                                        )
+                                        .responseFields(
+                                                fieldWithPath("statusCode").description("상태 코드").type(JsonFieldType.STRING),
+                                                fieldWithPath("message").description("메세지").type(JsonFieldType.NULL),
+                                                fieldWithPath("errorCode").description("에러코드").type(JsonFieldType.NULL),
+                                                fieldWithPath("data").description("데이터").type(JsonFieldType.NULL)
+                                        ).responseHeaders(
+                                                headerWithName(HttpHeaders.SET_COOKIE).description("쿠키 무효화")
+                                        ).build())
+                        )
+                );
+    }
+
+    @Test
+    void API_재발급() throws Exception {
+        //given
+        ReissueResponseDto responseDto = new ReissueResponseDto(TEST_ACCESS_TOKEN);
+
+        Mockito.when(authService.reissue(anyString(), anyString()))
+                .thenReturn(responseDto);
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                post("/auths/reissue")
+                        .header(HttpHeaders.AUTHORIZATION, TEST_ACCESS_TOKEN)
+                        .cookie(new Cookie(REFRESH_TOKEN_COOKIE_NAME, TEST_REFRESH_TOKEN))
+                        .contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.statusCode").value("OK"))
+                .andExpect(jsonPath("$.message").isEmpty())
+                .andExpect(jsonPath("$.errorCode").isEmpty())
+                .andExpect(jsonPath("$.data").isEmpty())
+                .andDo(restDocsHandler.document(
+                                ResourceDocumentation.resource(ResourceSnippetParameters.builder()
+                                        .tag("Auth")
+                                        .summary("재발급")
+                                        .description("- 재발급 입니다.")
+                                        .requestHeaders(
+                                                headerWithName(HttpHeaders.AUTHORIZATION).description("엑세스 토큰")
+                                        )
+                                        .responseFields(
+                                                fieldWithPath("statusCode").description("상태 코드").type(JsonFieldType.STRING),
+                                                fieldWithPath("message").description("메세지").type(JsonFieldType.NULL),
+                                                fieldWithPath("errorCode").description("에러코드").type(JsonFieldType.NULL),
+                                                fieldWithPath("data").description("데이터").type(JsonFieldType.NULL)
+                                        ).responseHeaders(
+                                                headerWithName(HttpHeaders.AUTHORIZATION).description("엑세스 토큰")
+                                        ).build())
+                        )
+                );
+    }
+}
