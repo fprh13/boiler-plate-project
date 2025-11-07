d67169654da04ec9dadd7af3e0e82c352f34aafd
조영무 <fprh13@gmail.com>
2025-11-07 20:50:31 +0900

    chore: 비즈니스와 서버 에러 로그 분리 #27

--- src/main/java/com/hello/boilerplate/global/exception/CustomExceptionHandler.java
+++ src/main/java/com/hello/boilerplate/global/exception/CustomExceptionHandler.java
@@ -3,6 +3,7 @@ package com.hello.boilerplate.global.exception;
 import static org.springframework.http.HttpStatus.*;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,6 +17,9 @@ public class CustomExceptionHandler {
 
 	@ExceptionHandler(CustomException.class)
 	protected ResponseEntity<ResponseDto<Void>> handleCustomException(CustomException e) {
+        if (e.getStatus().equals(HttpStatus.INTERNAL_SERVER_ERROR)) {
+            log.error(e.getMessage());
+        }
 		return ResponseEntity.status(e.getStatus()).body(ResponseDto.ofFail(e.getMessage()));
 	}
 
