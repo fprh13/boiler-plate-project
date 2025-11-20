d9be58d282a5a830d9eee0df0270bd8851cda033
조영무 <fprh13@gmail.com>
2025-11-20 22:03:14 +0900

    chore: 테스트 Async 동기 처리 #30

--- src/test/java/com/hello/module/IntegrationSupportTest.java
+++ src/test/java/com/hello/module/IntegrationSupportTest.java
@@ -1,11 +1,15 @@
 package com.hello.module;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.hello.boilerplate.support.config.AsyncTestConfig;
+
 @SpringBootTest
 @Transactional
+@Import(AsyncTestConfig.class)
 @ActiveProfiles("test")
 public abstract class IntegrationSupportTest {
 
